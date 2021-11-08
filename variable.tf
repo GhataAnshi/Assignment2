ed7e1de645a412d90f233040b4bf31a32157ed9a
GhataS <ghatasaxena27@gmail.com>
2021-11-08 16:08:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,27 +1,6 @@
-variable "aws_region" {}
-variable "aws_zones" {
- type        = list
- description = "List of availability zones to use"
- default     = ["ap-south-1a", "ap-south-1b"]
+variable "aws_region" {default = "ap-south-1"}
+
+variable "cluster-name" {
+  default = "levelup-tf-eks-demo"
+  type    = string
 }
-
-variable "AMIS" {
-    type = map
-    default = {
-        ap-south-1 = "ami-0fd48e51ec5606ac1"
-    }
-}
-
-variable "AWS_REGION" {
-  default = "ap-south-1"
-}
-
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
-}
-
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
-}
-
